2e7c1aad601e0269ac4acb2ca060ba78beea4434
ssentamuabel <ssentamuabel90@gmail.com>
2023-11-27 15:53:35 +0300

    first commit Implementation of patch for post Implementation of the csrf token

--- laravel-10-for-beginner/resources/views/profile/partials/user-avatar-form.blade.php
+++ laravel-10-for-beginner/resources/views/profile/partials/user-avatar-form.blade.php
@@ -11,18 +11,18 @@
 
  
 
-    <form method="post" action="/profile/avatar" class="mt-6 space-y-6">
-        @csrf
-        @method('patch')
+    <form method="post" action="{{route('profile.avatar')}}" class="mt-6 space-y-6">
+          <!-- <input type="hidden" name="_method" value="patch"> -->
+          @method('patch')
 
-      
+          <!-- <input type="hidden" name="_token" value="{{csrf_token()}}"> -->
+          @csrf
         <div>
             <x-input-label for="avatar" :value="__('Avatar')" />
             <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" :value="old('avatar', $user->avatar)" required autofocus autocomplete="avatar" />
             <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
         </div>
-
-       
+      
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
