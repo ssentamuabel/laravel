2b3606218162824779f1932a38d01a91c2c8e007
ssentamuabel <ssentamuabel90@gmail.com>
2023-11-27 17:16:52 +0300

    First commit

---
+++ laravel-10-for-beginner/app/Http/Controllers/Profile/AvatarController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AvatarController extends Controller
+{
+    //
+    public function update()
+    {
+        // 
+        return "Hello";
+    }
+}
