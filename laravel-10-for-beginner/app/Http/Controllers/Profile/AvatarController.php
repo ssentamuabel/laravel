7b3e0254c3f907f6f5ba3ffe69107b5276b4dacf
ssentamuabel <ssentamuabel90@gmail.com>
2023-11-28 09:44:43 +0300

    First commit How to give a response Response with a message How to display a message How to redirect

--- laravel-10-for-beginner/app/Http/Controllers/Profile/AvatarController.php
+++ laravel-10-for-beginner/app/Http/Controllers/Profile/AvatarController.php
@@ -10,7 +10,19 @@ class AvatarController extends Controller
     //
     public function update()
     {
-        // 
-        return "Hello";
+        // option 1
+        // return response()->redirectTo('/profile');
+
+        // Option 2
+        // return response()->redirectTo(route('profile.edit'));
+
+        // Option 3
+        // return back();
+
+        // Return with a response
+        return back()->with('message', 'Avatar is Added');
+
+        // Only redirecting
+        // return redirect(route('profile.edit'));
     }
 }
