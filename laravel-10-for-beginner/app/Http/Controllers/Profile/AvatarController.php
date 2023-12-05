07621c2327316d334a4acf6f28f9c8fe14e147c8
ssentamuabel <ssentamuabel90@gmail.com>
2023-12-05 10:39:35 +0300

    Commit

--- laravel-10-for-beginner/app/Http/Controllers/Profile/AvatarController.php
+++ laravel-10-for-beginner/app/Http/Controllers/Profile/AvatarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Support\Facades\Storage;
 
 
 class AvatarController extends Controller
@@ -13,7 +14,17 @@ class AvatarController extends Controller
     public function update(UpdateAvatarRequest $request)
     {
        
-      $path = $request->file('avatar')->store('avatars','public' );
+    //   $path = $request->file('avatar')->store('avatars','public' );
+
+    // alternatively
+    $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+
+
+      // delete the older avatar if it exists
+      if ($oldAvatar = $request->user()->avatar)
+      {
+        Storage::disk('public')->delete($oldAvatar);
+      }
 
 
 
